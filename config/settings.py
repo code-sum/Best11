0a1ab056a2ea5bf5405cb71918ee0755821afebc
code-sum <yisugyeong@gmail.com>
2022-11-27 11:57:13 +0900

    accounts 앱 생성 및 URL 분리

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',  # 11/27 등록(이수경)
     'django_bootstrap5',  # 11/27 등록(이수경)
     'django.contrib.admin',
     'django.contrib.auth',
