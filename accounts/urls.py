f5a0f158a1c23c048be53248a14cf67543863e09
JeongJinGan <rksaudwls123@naver.com>
2022-11-28 16:05:58 +0900

    feat: acoounts 모델 생성 및, 로그인, 로그아웃, 내정보, 탈퇴

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
-app_name = 'accounts'
+app_name = "accounts"
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("detail/<int:pk>/", views.detail, name="detail"),
+    path("delete/", views.delete, name="delete"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+
 
 def index(request):
-    return render(request, 'accounts/index.html')
+    accounts = get_user_model().objects.order_by("-pk")
+    context = {"accounts": accounts}
+    return render(request, "accounts/index.html", context)
+
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect("accounts:index")
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            # 주소 수정 필요
+            return redirect(request.GET.get("next") or "accounts:index")
+    else:
+        form = AuthenticationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/login.html", context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect("accounts:login")
+
+
+def detail(request, pk):
+    user = get_user_model().objects.get(pk=pk)
+    context = {
+        "user": user,
+    }
+    return render(request, "accounts/detail.html", context)
+
+
+def delete(request):
+    request.user.delete()
+    auth_logout(request)
+    return redirect("accounts:index")
