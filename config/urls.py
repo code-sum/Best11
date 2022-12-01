3096b8e0dc54e06cd80bb2e73fcfcc21252f08f6
JOO-10000-Zi <j4615545@gmail.com>
2022-12-01 09:47:36 +0900

    메인 인덱스 코리아인덱스 연결

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('korea/', include('korea.urls')),
-    path('summernote/', include('django_summernote.urls')),
+    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
+    path("accounts/", include("accounts.urls")),
+    path("korea/", include("korea.urls")),
+    path("summernote/", include("django_summernote.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
